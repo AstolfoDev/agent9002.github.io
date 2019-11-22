f0572fb53f7152d57cc304fda399650fe60a6d26
Tommy Richardson <digitalgpro@gmail.com>
2019-11-22 15:28:46 +0000

    • Media Updates • Added PFP for Nathan and Blitz

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function nate(){
   $('#nate').css('opacity', 0);
   setTimeout(function(){ document.getElementById("nate").textContent = "@natejb2003#5242";$('#nate').css('opacity', 1);}, 450);
 }
+
+function tommyimage() {
+
+        if (document.getElementById("tommy1").src == "https://i.postimg.cc/SKjk56pS/ACCA2168-414-D-44-E5-899-C-4-A46073-AF89-D.jpg") 
+        {
+            document.getElementById("tommy1").src = "assets/img/original.gif";
+            tommy1.height = 160;
+            tommy1.width = 160;
+        }
+        else 
+        {
+            document.getElementById("tommy1").src = "https://i.postimg.cc/SKjk56pS/ACCA2168-414-D-44-E5-899-C-4-A46073-AF89-D.jpg";
+        }
+    }

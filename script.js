763e7dafe2b939271e42251bfcae9448f4d15142
Tommy Richardson <digitalgpro@gmail.com>
2019-11-22 11:33:02 +0000

    * Updated Team List * Updated About Us

---
+++ script.js
@@ -0,0 +1,14 @@
+function tommyfunction(){
+  $('#tommy').css('opacity', 0);
+  setTimeout(function(){ document.getElementById("tommy").textContent = "@agent#7574";$('#tommy').css('opacity', 1);}, 450);
+}
+
+function casper(){
+  $('#casper').css('opacity', 0);
+  setTimeout(function(){ document.getElementById("casper").textContent = "@Stigma#1310";$('#casper').css('opacity', 1);}, 450);
+}
+
+function nate(){
+  $('#nate').css('opacity', 0);
+  setTimeout(function(){ document.getElementById("nate").textContent = "@natejb2003#5242";$('#nate').css('opacity', 1);}, 450);
+}
